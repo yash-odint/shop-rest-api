e4d4e2fd3b93a47a88e1f46ba939112d051e7e55
Yashwant Soni <yashodint@gmail.com>
2024-02-07 13:19:35 +0530

    handle route errors

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
 const express = require('express');
 const app = express();
+const morgan = require("morgan");
+
 
 const productRoutes = require("./api/routes/products");
-app.use("/products", productRoutes);
-
 const orderRoutes = require("./api/routes/orders");
+
+app.use(morgan('dev'));
+
+//routes handle
+app.use("/products", productRoutes);
 app.use("/orders", orderRoutes);
+
+//error catch
+app.use((req, res, next) => {
+    const error = Error("Not found");
+    error.status = 404;
+    next(error);
+});
+//error handling
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error : {
+            message: error.message
+        }
+    });
+});
+
+
 
 module.exports = app;
