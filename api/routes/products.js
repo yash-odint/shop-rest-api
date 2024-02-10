cf1cfde63625902f61e56e4cf63ea37d2a0f813d
Yashwant Soni <yashodint@gmail.com>
2024-02-11 00:56:23 +0530

    "added validations"

--- api/routes/products.js
+++ api/routes/products.js
@@ -6,8 +6,29 @@ const Product = require("../models/product");
 //all routes will be placed after "/products"
 //handling "/products/"
 router.get("/", (req, res, next) => {
-    res.status(200).json({
-        message: "handling GET request to /products"
+    Product.find().select("price name _id")
+    .exec()
+    .then((docs) => {
+        const response = {
+            count: docs.length,
+            products: docs.map((doc)=>{
+                return {
+                    _id: doc._id,
+                    name: doc.name,
+                    price: doc.price,
+                    request: {
+                        type: "GET",
+                        url: "http://localhost:9999/products/"+doc._id
+                    }
+                }
+            })
+        }
+        res.status(200).json(response);
+    })
+    .catch((err) => {
+        res.status(500).json({
+            error: err
+        });
     });
 });
 router.post("/", (req, res, next) => {
@@ -19,10 +40,17 @@ router.post("/", (req, res, next) => {
     product
         .save()
         .then((result)=>{
-            console.log(result);
             res.status(200).json({
-                message: "handling POST request to /products",
-                product: product
+                message: "Product created successfully",
+                product: {
+                    _id: result._id,
+                    name: result.name,
+                    price: result.price,
+                    request: {
+                        type: "GET",
+                        url: "http://localhost:9999/products/"+result._id
+                    }
+                }
             });
         })
         .catch((err)=>{
@@ -34,7 +62,7 @@ router.post("/", (req, res, next) => {
 //handling "/products/{id}"
 router.get("/:productId", (req, res, next)=>{
     const id = req.params.productId;
-    Product.findById(id)
+    Product.findById(id).select("price _id name")
         .exec()
         .then((doc)=>{
             if(doc){
@@ -53,14 +81,42 @@ router.get("/:productId", (req, res, next)=>{
 });
 
 router.patch("/:productId", (req, res, next) => {
-    res.status(200).json({
-        message: "product updated"
-    });
+    const id = req.params.productId;
+    const updateOps = {};
+    //[{propName: "name", value: "harry potter"}]
+    for(const ops of req.body){
+        updateOps[ops.propName] = ops.value; 
+    }
+    Product.updateOne({_id: id}, { $set: updateOps})
+        .exec()
+        .then((result)=>{
+            const response = {
+                message: "Product updated",
+                request: {
+                    type: "GET",
+                    url: "http://localhost:9999/products/"+id
+                }
+            }
+            res.status(200).json(response);
+        })
+        .catch((err) => {
+            res.status(500).json({
+                error: err
+            })
+        });
 });
 
 router.delete("/:productId", (req, res, next) => {
-    res.status(200).json({
-        message: "product deleted"
+    const id = req.params.productId;
+    Product.deleteOne({_id:id})
+    .exec()
+    .then((result) => {
+        res.status(200).json({res: result});
+    })
+    .catch((err) => {
+        res.status(500).json({
+            error: err
+        });
     });
 });
 
--- app.js
+++ app.js
@@ -3,13 +3,11 @@ const app = express();
 const morgan = require("morgan");
 const bodyparser = require("body-parser");
 const mongoose = require("mongoose");
+require('dotenv').config();
 
 const productRoutes = require("./api/routes/products");
 const orderRoutes = require("./api/routes/orders");
-
-mongoose.connect(
-    "mongodb+srv://yashodint:yashodint@shop-rest-api.8n3no4v.mongodb.net/?retryWrites=true&w=majority"
-);
+mongoose.connect(process.env.MONGO_DB_URL);
 
 // http request logger
 app.use(morgan('dev'));
